c117a1a1fa0cc1b7a9bd2aaab28f36767234ee9a
KyrieNO1 <976906494@qq.com>
2023-11-08 13:30:40 +0800

    example code

--- class18/动态类和方法.py
+++ class18/动态类和方法.py
@@ -6,4 +6,5 @@ class Student:
 
 stu1 = Student('tom', 20)
 stu1.sex = 'male'
+#在定义后可以为对象添加属性
 print(f'{stu1.name}: {stu1.age}: {stu1.sex}')
--- class18/可见性.py
+++ class18/可见性.py
@@ -3,7 +3,7 @@ class Student:
     def __init__(self, name, age):
         self.__name = name
         self.__age = age
-
+#__.name/__.age无法在对象直接使用，但是在类内的方法可以使用
     def study(self, course):
         print(f'{self.__name} is studying {course}')
 
@@ -11,4 +11,5 @@ stu1 = Student('tom', 20)
 
 stu1.study('python')
 #print(stu1.__name)
+
 print(stu1._Student__name)
